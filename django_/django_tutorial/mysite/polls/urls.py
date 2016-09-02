7545c9921b27ff2cb40a2131864772a522583a8b
F3285405 <oliver.loe@fnb.co.za>
2016-09-02 15:56:37 +0200

    Continued working on django tutorial. Implemented templates

--- django_/django_tutorial/mysite/polls/urls.py
+++ django_/django_tutorial/mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # example: /polls/
     url(r'^$', views.index, name='index'),
---
+++ find-duplicates/find_duplicates.py
@@ -0,0 +1,23 @@
+fin = open("numbers.txt", "rt")
+
+numbers = fin.readlines()
+
+fin.close()
+
+unique_list = []
+
+# Let's iterate through all the numbers and root out any duplicates.
+for num in numbers:
+    if num not in unique_list:
+        unique_list.append(num)
+
+print("Numbers Size: ", len(numbers))
+print("Unique List Size: ", len(unique_list))
+
+if len(numbers) == len(unique_list):
+    print("Numbers are unique")
+else:
+    print("Numbers have duplicates")
+
+
+
