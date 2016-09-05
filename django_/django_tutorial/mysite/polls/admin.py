31b1c2a7dbd0ae3c8a5e26073436b6b70700082a
F3285405 <oliver.loe@fnb.co.za>
2016-09-05 18:20:43 +0200

    Worked on how to customize the django admin site, and also created a docker container using the django:onbuild image with the requirements.txt file

--- django_/django_tutorial/mysite/polls/admin.py
+++ django_/django_tutorial/mysite/polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text'] # Change the order of fields to be displayed
+
+    # Define different categorizations for your fields
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information:', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently') # This property displays more columns when viewing the Quesiton in the admin site.
+
+    # Provides a filter option on the pub_date field.
+    list_filter = ['pub_date']
+
+    # Provide search capablilities on the admin site for the question_text field
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
 
--- django_/django_tutorial/mysite/polls/models.py
+++ django_/django_tutorial/mysite/polls/models.py
@@ -4,7 +4,7 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
@@ -12,6 +12,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    # These fields are used by the django admin site to provide sort capabilities, glyph icon, and a column header
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
